8794e55822db64c4381745a6b72daa3b0160fb31
Petar Ozretic <petar.ozretic@locastic.com>
2019-07-18 17:22:13 +0200

    Refactor cloud functions to use express

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,36 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const express = require('express');
 
 admin.initializeApp();
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => response.send('Hello world'));
-
-exports.getWorkshops = functions.https.onRequest((req, res) => {
+app.get('/workshops', (req, res) => {
   admin
     .firestore()
     .collection('workshops')
+    .orderBy('createdAt', 'desc')
     .get()
     .then((data) => {
       const workshops = [];
       data.forEach((doc) => {
-        workshops.push(doc.data());
+        workshops.push({
+          workshopID: doc.id,
+          title: doc.data().title,
+          department: doc.data().department,
+          createdAt: doc.data().createdAt,
+        });
       });
       return res.json(workshops);
     })
     .catch(err => console.error(err));
 });
 
-exports.createWorkshop = functions.https.onRequest((req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(400).json({ error: 'Method not allowed!' });
-  }
+app.post('/workshop', (req, res) => {
   const newWorkshop = {
     title: req.body.title,
     department: req.body.department,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
   };
 
   admin
@@ -39,7 +39,9 @@ exports.createWorkshop = functions.https.onRequest((req, res) => {
     .add(newWorkshop)
     .then(doc => res.json({ message: `Document ${doc.id} created successfully` }))
     .catch((err) => {
-      res.status(500), json({ error: 'Something went wrong' });
+      res.status(500).json({ error: 'Something went wrong' });
       console.log(err);
     });
 });
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
