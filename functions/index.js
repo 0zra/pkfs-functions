71f234549fbe9f4dd834ecfde0ed088e9582b937
Petar Ozretic <petar.ozretic@locastic.com>
2019-07-18 11:25:46 +0200

    Add getWorkshops & createWorkshop cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,45 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => response.send('Hello world'));
+
+exports.getWorkshops = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection('workshops')
+    .get()
+    .then((data) => {
+      const workshops = [];
+      data.forEach((doc) => {
+        workshops.push(doc.data());
+      });
+      return res.json(workshops);
+    })
+    .catch(err => console.error(err));
+});
+
+exports.createWorkshop = functions.https.onRequest((req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(400).json({ error: 'Method not allowed!' });
+  }
+  const newWorkshop = {
+    title: req.body.title,
+    department: req.body.department,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection('workshops')
+    .add(newWorkshop)
+    .then(doc => res.json({ message: `Document ${doc.id} created successfully` }))
+    .catch((err) => {
+      res.status(500), json({ error: 'Something went wrong' });
+      console.log(err);
+    });
+});
