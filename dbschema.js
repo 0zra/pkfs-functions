2fa7f40afebfb226915bb946f923b3713454f7b2
Petar Ozretic <petar.ozretic@locastic.com>
2019-07-18 17:21:09 +0200

    Create a db schema

---
+++ dbschema.js
@@ -0,0 +1,10 @@
+let db = {
+    workshops: [
+        {
+            title: "naslov",
+            department: "med",
+            createdAt: "2019-03-15T11:46:01.018Z",
+            //  maxAtendee, aplications, years
+        }
+    ]
+}
