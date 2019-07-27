2f069cf98a58af4cf28cf6a2e3fb4d7d90912f43
Petar Ozretic <petar.ozretic@locastic.com>
2019-07-27 06:57:39 +0200

    Add firebase package

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
 
 admin.initializeApp();
-const app = express();
+const app = require('express')();
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDWHM957dc4Qq8aU4C2HEWS71GBYyvn-Jg',
+  authDomain: 'pkfs-1950.firebaseapp.com',
+  databaseURL: 'https://pkfs-1950.firebaseio.com',
+  projectId: 'pkfs-1950',
+  storageBucket: 'pkfs-1950.appspot.com',
+  messagingSenderId: '635726540094',
+  appId: '1:635726540094:web:b3ee9122fc0277b1',
+};
+
+const firebase = require('firebase');
+
+firebase.initializeApp(firebaseConfig);
 
 app.get('/workshops', (req, res) => {
   admin
